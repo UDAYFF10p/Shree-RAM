cdcd433b64df4bf8921e7f79d9529744e32b899b
UDAYFF10p <sharmaudayraj0596@gmail.com>
2025-08-16 00:24:45 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBe1-wz4--Ey_UETXe_WFB8XWhPBNybt_0",
+  authDomain: "shree-ram-bc751.firebaseapp.com",
+  projectId: "shree-ram-bc751",
+  storageBucket: "shree-ram-bc751.firebasestorage.app",
+  messagingSenderId: "992991717018",
+  appId: "1:992991717018:web:99c983c3e7c6e3b976e123",
+  measurementId: "G-G1WT7XKCSF"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
